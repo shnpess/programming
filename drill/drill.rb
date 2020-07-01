a7c44fcbbabc4dde76ebadea2f88e971765e5136
shnpess <ann2lax@gmail.com>
2020-07-01 13:03:56 +0900

    tech  #29

--- drill/drill.rb
+++ drill/drill.rb
@@ -1,12 +1,42 @@
-input = [3, 5, 9 ,12, 15, 21, 29, 35, 42, 51, 62, 78, 81, 87, 92, 93]
-num = gets.to_i
-if input.index(num)
-   index_number = input.index(num)
-   puts "#{num}は#{index_number}番目にあります"
-else
-   puts "#{num}はありません"
+# 配列の内部に、複数のユーザーの情報をハッシュとして持つ変数user_dataがあります。
+
+user_data = [
+  {
+    user: {
+        profile: {
+            name: 'George'
+        }
+    }
+  },
+  {
+    user: {
+        profile: {
+          name: 'Alice'
+        }
+    }
+  },
+  {
+    user: {
+        profile: {
+            name: 'Taro'
+        }
+    }
+  }
+]
+# user_dataを利用して、全てのユーザーの名前だけが出力されるようにRubyでコーディングしてください。
+# ただし、出力結果は次のようになるものとします。
+
+# ターミナル
+# George
+# Alice
+# Taro
+
+# ---------------------------解答--------------------------------------------
+
+user_data.each  do |data|
+   user_info = data[:user]
+   user_prof = user_info[:profile]
+   puts "#{user_prof[:name]}"
 end
-
-
 
 
