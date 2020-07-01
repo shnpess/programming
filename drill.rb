673e4421d0a2323cb94875917997b822bbbeba19
shnpess <ann2lax@gmail.com>
2020-07-01 17:33:25 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -1,15 +1,18 @@
-# https://paiza.jp/works/mondai/prob60/5_or_more_9?language_uid=ruby
+# https://paiza.jp/works/mondai/prob60/total_value_9?language_uid=ru
 
 # 解答
 
 count = gets.to_i
 
 sum = 0
+
 count.times do
-  num = gets.to_i 
-  if num >= 5
-    sum += num
+  num = gets.split.map(&:to_i)
+  if num[0] == num[1]
+     sum += num[0] * num[1]
+  else
+     sum += (num[0] + num[1])
   end
 end
 
-puts "#{sum}"
+puts sum
