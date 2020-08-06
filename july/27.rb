7595b1f983d75da8d1dee4ccb94a10d4226cbab7
shnpess <ann2lax@gmail.com>
2020-08-06 23:44:16 +0900

    Update 27.rb

--- july/27.rb
+++ july/27.rb
@@ -25,3 +25,48 @@ count = 0
     end
 end
 puts usagi
+# https://paiza.jp/works/challenges/389/retry
+input = gets.to_i
+
+shoes = []
+input.times do
+   kutu = gets.split.map(&:chomp)
+   shoes << kutu
+end
+
+type = []
+shoes.each do |s|
+    type << s[0]
+end
+type = type.uniq
+
+
+
+answer = []
+count = 0
+type.length.times do
+    aaa = []
+    shoes.each do |s|
+        if s[0] == type[count]
+            aaa << s
+        end
+    end
+
+    r = []
+    l = []
+    aaa.each do |a|
+        if a[1] == "R"
+        r << a
+        elsif a[1] == "L"
+        l << a
+        end
+    end
+
+    if r.length < l.length
+        answer << r.length
+    else
+        answer << l.length
+    end
+    count += 1
+end
+p answer.sum
